3d1a802e7cfe60112941b6f73f8176217cd9a78b
Paul Weder <112962227+Pweder3@users.noreply.github.com>
2026-06-20 15:47:42 -0400

    inital config DTS should be finished and most of the app code should be done main loop is left

--- README.md
+++ README.md
@@ -8,12 +8,23 @@ Repo is for the firmware for the strain gauge board
 ## Extra Scope 
 * calculates forces and passes force values to VCU
   * Could be offloaded later by copying functionality since SG MCU is very capable.
+* implement ADC DMA streaming and increase the sampling/avg rate to increase effective accuracy.
+* Get actual reference voltage from ADC pin to increase ADC accuracy.
  
+
+### Scope Architecture  
+- [ ] Sampling Thread
+  * sample x amount of data and calculate running avg and pass to message Queue 
+- [ ] CAN Thread
+  * take queued data and send CAN 
+    * need to wait for 3 messages worth and in the correct order 
+
 
 
 # TODO
-* DTS file
-* wrapper/interface for ADC's (functional) to get voltages
-* logging for debuging
-* Decode to 8bit 
-* Send to VCU 
+- [x] DTS file
+- [ ] Sampling function
+- [ ] averaging function 
+- [ ] Can message function
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <math.h>
+#include <stdlib.h>
+#include <sys/errno.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/can.h>
 #include <syscalls/can.h>
@@ -19,7 +21,7 @@
 #include <zephyr/toolchain.h>
 #include <zephyr/types.h>
 #include "can.h"
-
+#include <zephyr/drivers/adc.h>
 
 #ifndef __cplusplus
 #error "__cplusplus not defined! Build system is compiling as C!"
@@ -52,30 +54,130 @@ void send_heartbeat(CanBus can)
     heartbeat_counter++;
 }
 
-constexpr uint32_t strainGuageID = 0x600; //TODO: get right ID
+constexpr uint32_t BASE_SG_CAN_ID = 0x600; //TODO: get right ID
+constexpr uint8_t NUM_CH = 9;
+constexpr uint8_t SMP_AVG_WINDOW = 3;
+
+uint16_t samples[NUM_CH][SMP_AVG_WINDOW];
+uint16_t canPayload[NUM_CH];
+uint16_t pakGrpCounter = 0; 
 
 
+void addSample(uint8_t chNum, uint16_t sample){
 
-can_frame genChCanFrame(uint8_t chNum,float_t payload){
+    if (chNum > NUM_CH){
+        LOG_ERR("Channel Number must be valid");
+        return;
+    }
 
-    struct can_frame retFrame = {.id = strainGuageID,
-                                 .dlc = 6,
+
+    for (int i =SMP_AVG_WINDOW; i>0;i--){
+        samples[chNum][i] = samples[chNum][i-1];
+    }
+    samples[chNum][0] = sample;
+}
+
+uint16_t getAvgSample(uint8_t chNum){
+
+    if (chNum > NUM_CH){
+        LOG_ERR("Channel Number must be valid");
+        return 0;
+    }
+
+    double sum=0;
+
+    for (int i =0; i< SMP_AVG_WINDOW;i++){
+        sum += samples[chNum][i];
+    }
+
+    sum/= SMP_AVG_WINDOW;
+
+    return static_cast<uint16_t>(sum);
+
+}
+
+can_frame genChCanFrame(uint8_t packetNum){
+
+    if (packetNum > 3){
+        LOG_ERR("Packet Number must be valid");
+        struct can_frame retFrame = {.id = 0,
+                                     .dlc = 0,
+                                     .flags = 0,
+                                     .data = {0,0,0,0,0,0,0,0}};
+        return retFrame;
+    }
+
+
+    /*
+    Packet 1 -> SG1-SG3
+    Packet 2 -> SG4-SG6
+    Packet 3 -> SG7-SG9
+    */
+
+    uint16_t pay1 = canPayload[packetNum*3-3];
+    uint16_t pay2 = canPayload[packetNum*3-2];
+    uint16_t pay3 = canPayload[packetNum*3-1];
+
+
+    if (packetNum == 3) pakGrpCounter ++;
+
+    if (pakGrpCounter == 65535) LOG_WRN("Packet Group Counter Overflow");
+
+    struct can_frame retFrame = {.id = (BASE_SG_CAN_ID +packetNum-1),
+                                 .dlc = 8,
                                  .flags = 0,
-                                 .data =  }; //TODO: parse channel # and payload data into can frame 1 byte - ch #, 4 bytes data
+                                 .data = {(uint8_t)(pay1 & 0xFF),(uint8_t)(pay1 << 8),
+                                          (uint8_t)(pay2 & 0xFF),(uint8_t)(pay2 << 8),
+                                          (uint8_t)(pay3 & 0xFF),(uint8_t)(pay3 << 8),
+                                          (uint8_t)(pakGrpCounter & 0xFF),(uint8_t)(pakGrpCounter << 8)}};
 
-
+    return retFrame;
 }
 
 
 
 
-int main(void)
-
-{
+int main(void){
 
     LOG_INF("Main Innit");
 
     const struct device *can_dev = DEVICE_DT_GET(DT_NODELABEL(fdcan1));
+
+    
+    static const struct adc_dt_spec strainChannels[NUM_CH] = {
+        ADC_DT_SPEC_GET_BY_NAME(DT_PATH(zephyr_user), sg1),
+        ADC_DT_SPEC_GET_BY_NAME(DT_PATH(zephyr_user), sg2),
+        ADC_DT_SPEC_GET_BY_NAME(DT_PATH(zephyr_user), sg3),
+        ADC_DT_SPEC_GET_BY_NAME(DT_PATH(zephyr_user), sg4),
+        ADC_DT_SPEC_GET_BY_NAME(DT_PATH(zephyr_user), sg5),
+        ADC_DT_SPEC_GET_BY_NAME(DT_PATH(zephyr_user), sg6),
+        ADC_DT_SPEC_GET_BY_NAME(DT_PATH(zephyr_user), sg7),
+        ADC_DT_SPEC_GET_BY_NAME(DT_PATH(zephyr_user), sg8),
+        ADC_DT_SPEC_GET_BY_NAME(DT_PATH(zephyr_user), sg9),
+    };
+
+    for (adc_dt_spec ch : strainChannels){
+        int ret =adc_channel_setup_dt(&ch);
+
+        if (ret == EINVAL){
+            LOG_ERR("ADC channel %i failed to setup",ch.channel_id);
+        }
+    }
+
+    int16_t buf;
+    struct adc_sequence seq = {
+        .buffer = &buf,
+        .buffer_size = sizeof(buf),
+    };
+
+    for (int i = 0; i < NUM_CH; i++) {
+        adc_sequence_init_dt(&strainChannels[i], &seq);
+        int ret = adc_read_dt(&strainChannels[i], &seq);
+        if (ret == 0) {
+            canPayload[i] = (uint16_t)buf;
+        }
+    }
+
 
     CanBus can;
     can.init(can_dev);
@@ -83,5 +185,8 @@ int main(void)
 
 
 
+
+
+
     k_sleep(K_FOREVER);
-}
+};
